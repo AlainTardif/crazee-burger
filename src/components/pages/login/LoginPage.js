33ac8648073be65b096c9da313ad8565becf5c31
Alain Tardif <tardif.alain@gmail.com>
2023-12-08 16:19:21 +0100

    ♻️ F01: LOGIN > Create LoginForm

---
+++ src/components/pages/login/LoginPage.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import LoginForm from "./LoginForm";
+
+export const LoginPage = () => {
+ 
+  // affichage (render)
+  return <LoginForm/>
+};
+
+export default LoginPage;
